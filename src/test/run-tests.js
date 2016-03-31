d460b048dfde2b58d1af2c29103c0d1308ae03f2
Pietro Martinelli <amicofragile@gmail.com>
2016-03-31 14:35:02 +0200

    TEST Use Jasmine's TerminalReport, with full verbosity and colors

--- src/test/run-tests.js
+++ src/test/run-tests.js
@@ -1,8 +1,8 @@
 var Jasmine = require('jasmine');
 var jasmine = new Jasmine();
+var reporters = require('jasmine-reporters');
+var consoleReporter = new reporters.TerminalReporter({ verbosity: 3, color: true });
 
 jasmine.loadConfigFile('spec/support/jasmine.json');
-jasmine.configureDefaultReporter({
-    showColors: true
-});
+jasmine.addReporter(consoleReporter);
 jasmine.execute();
